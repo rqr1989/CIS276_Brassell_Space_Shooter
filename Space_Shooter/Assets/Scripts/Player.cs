e6bd931f4cc267615c47eef6415caae234a104c5
rqr1989 <dontopenthepyxis@gmail.com>
2022-02-22 21:42:14 -0500

    Skyboxes Added

---
+++ Space_Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Player Ship
+//Can move in all cardinal directions (up down left right)
+//Can fire shots by clicking mouse or pressing space bar that move forward until off screen or colliding with an enemy
+//2 different weapon types that can be switched by pressing a button/key (these could be shots with different trajectories, sizes, speeds, different behavior, etc.)
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
