eefc9f60b6583add9b852f3a2fade0430b1e20d6
rqr1989 <dontopenthepyxis@gmail.com>
2022-03-03 22:05:44 -0500

    Spawn at random times

--- Space_Shooter/Assets/Scripts/EnemySpawn.cs
+++ Space_Shooter/Assets/Scripts/EnemySpawn.cs
@@ -8,16 +8,17 @@ public class EnemySpawn : MonoBehaviour
     public float rightBoundry = 37.63396f;
     public float leftBoundry = -34.95398f;
     public float topBoundry = 25.55841f;
-    public float bottomBoundry = 5f;
+    public float bottomBoundry = 15f;
     public int Health = 15;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed = 5f;
-    public float lowestSpawnPoint = 15;
-    public int spawnLow = 1;
-    public int spawnHigh = 4;
-    public float spawnTimeLow = 1f;
-    public float spawnTimeHigh = 3f;
-
+    public float lowestSpawnPoint = 5;
+    public float spawnTimeLow = 2f;
+    public float spawnTimeHigh = 6f;
+    public float spawnDelay = 1f;
+    int numberOfEnemys;
+   
+    ///public ;
     // Start is called before the first frame update
     void Awake()
     {
@@ -26,17 +27,28 @@ public class EnemySpawn : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        GameObject enemy;
+        float spawnTime = Random.Range(1,4);
+        InvokeRepeating("EnemyShipSpawn", spawnDelay, spawnTime);
+    }
+
+    // Update is called once per frame
+    void EnemyShipSpawn()
+    {
+        
+       
+         GameObject enemy;
+     
         //creates enemy instance
         enemy = Instantiate(enemyShip);
-     
+
+       
         //spawns enemy within top half of screen
         enemy.transform.position = new Vector3(Random.Range(leftBoundry, rightBoundry), Random.Range(lowestSpawnPoint, topBoundry), 160f);
-        spawnEnemy();
+    
 
+        numberOfEnemys = Random.Range(1, 4);
         //move enemy ship down by 1
         Vector3 movement = new Vector3(0, 1, 0);
         enemy.transform.Translate(movement * speed * Time.deltaTime);
@@ -54,19 +66,11 @@ public class EnemySpawn : MonoBehaviour
         {
             Destroy(enemy);
         }
-      
+
 
     }
+ 
+  
+  
 
-    void spawnEnemy()
-    {
-        StartCoroutine("SpawnTime");
-    }
-
-    IEnumerator SpawnTime()
-    {
-        
-            yield return new WaitForSeconds(Random.Range(1f, 3f));
-    }
-    
 }
